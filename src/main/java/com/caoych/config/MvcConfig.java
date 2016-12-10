c0c78ea95e7daedcc18b2d5f33255fbc4266bd3c
Jostar1024 <howard.eureka@gmail.com>
2016-12-10 15:58:44 +0100

    implement spring boot security and make session worked

---
+++ src/main/java/com/caoych/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.caoych.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by caoych on 2016/12/10.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
